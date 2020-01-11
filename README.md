8f1829643d60d56744c964f749e276ac60384fbc
Julius <julius.rauscher@uni-konstanz.de>
2020-01-11 16:39:57 +0100

    enable i2c device

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 # hifihat
 files to operate hifihat with a raspberry pi.
 
-Important: This repo assumes the pi user is present and copies the files into that home directory
+Important: This repo assumes the pi user is present and copies the files into that home directory.
 
 ## install
-copy the files from the boot folder into the boot partition of the SD card
+copy the files from the boot folder into the boot partition of the SD card. Copy your WiFi credentials into the wpa_supplicant.conf file first.
 
 run the init.sh file
+```Bash
+./init.sh
+```
+
--- init.sh
+++ init.sh
@@ -2,20 +2,21 @@
 
 sudo apt-get -y install wiringpi i2c-tools libi2c-dev
 
-#manual requirements:
 #   enable i2c via sudo raspi-config
+echo "dtparam=i2c_arm=on" >> /boot/config.txt
+echo "dtparam=i2c1=on" >> /boot/config.txt
 
 #   add "sudo modprobe i2c-bcm2708" to etc/rc.local
 echo "sudo modprobe i2c-bcm2708" >> /etc/rc.local
 
 
 #add i2c-bcm2708 && i2c-dev to /etc/modules
+echo "i2c-bcm2708" >> /etc/modules
+echo "i2c-dev" >> /etc/modules
 
 #make cc for changing coefficients executable in any directory
-sudo /home/pi/hifihat/socket/cp cc /usr/local/bin/cc
-
-
+sudo cp socket/cc /usr/local/bin/cc
 
 #create service
-sudo cp hifihat.service /etc/systemd/system/hifihat.service
+sudo cp hifihat/hifihat.service /etc/systemd/system/hifihat.service
 sudo systemctl enable hifihat
